bbbc8a3257fd37bce31cb37d5537c4c27151d6ce
ygorarevalo-rgb <ygor.arevalo@ufms.br>
2026-07-15 13:57:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function mensagem(){
-    alert("Bem-vindo ao meu site!");
-}
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function () {
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
